f4f7a1acbec1b5e7e62c6c44fc81816c9b1e20cb
Tim Kempkens <tim.kempkens@googlemail.com>
2022-06-06 17:43:49 +0200

    started timer refactoring

---
+++ src/models/timer.class.ts
@@ -0,0 +1,49 @@
+import { BehaviorSubject } from "rxjs";
+
+export class Timer {
+
+  secondsLeft!: number;
+  timerStarted = false;
+  setIntervalRef!: any;
+  timerFinished = new BehaviorSubject(false);
+  
+  constructor(secondsLeft?: number){
+    this.secondsLeft = secondsLeft ? secondsLeft : 10; 
+  }
+
+  removeSecond() {
+    this.secondsLeft--;
+    if (this.isTimerFinished()) {
+      this.finishTimer();
+    }
+  }
+
+  finishTimer() {
+    this.timerFinished.next(true);
+    this.pause();
+  }
+
+  isTimerFinished() {
+    return this.secondsLeft == 0;
+  }
+
+  play() {
+    if (this.timerStarted) return;
+    this.timerStarted = true;
+    this.setIntervalRef = setInterval(() => {this.removeSecond()}, 1000);
+  }
+
+  pause() {
+    this.timerStarted = false;
+    clearInterval(this.setIntervalRef);
+  }
+
+  convertTimeToClock(): [number, number] {
+    return [this.secondsLeft % 60, this.secondsLeft - (this.secondsLeft % 60)*60];
+  }
+
+  addFiveMinutes() {
+    this.secondsLeft += 5*60;
+  }
+
+}
