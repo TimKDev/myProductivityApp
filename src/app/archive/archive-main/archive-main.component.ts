bff5b06577eb943b04d0c0b602db1bd73a13c83a
Tim Kempkens <tim.kempkens@googlemail.com>
2022-06-26 12:57:35 +0200

    fixed Bug Positions when tasks is deleted

--- src/app/archive/archive-main/archive-main.component.ts
+++ src/app/archive/archive-main/archive-main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDetailsComponent } from 'src/app/kanban/task-details/task-details.component';
+import { MyTask } from 'src/models/task.class';
 
 @Component({
   selector: 'app-archive-main',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchiveMainComponent implements OnInit {
 
-  constructor() { }
+  archivedTasks: any = [];
+  allBoards: any = [];
+
+  constructor(
+    private firestore: AngularFirestore,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+    this.firestore
+    .collection('archive')
+    .valueChanges({idField: 'taskId'})
+    .subscribe((changes: any) => {
+      this.archivedTasks = changes.sort((task1: any, task2: any) => task2.dueDate - task1.dueDate);
+    });
+
+    this.firestore
+    .collection('boards')
+    .valueChanges({idField: 'boardId'})
+    .subscribe((changes: any) => {
+      this.allBoards = changes;
+    });
+  }
+
+  isMobileView() {
+    return window.innerWidth < 1000;
+  }
+
+  openDialogTasksDetails(taskId: string, currentTask: MyTask){
+    const dialogRef = this.dialog.open(TaskDetailsComponent, {
+      maxWidth: this.isMobileView() ? '100vw' : 'auto',
+      maxHeight: this.isMobileView() ? '100vh' : 'auto',
+      height: this.isMobileView() ? '100%' : 'auto',
+      width: this.isMobileView() ? '100%' : 'auto'
+    }); 
+    dialogRef.componentInstance.taskId = taskId;
+    dialogRef.componentInstance.currentTask = currentTask;
+    dialogRef.componentInstance.activeBoard = this.allBoards.find((board: any) => currentTask.boardName == board.boardId);
+  }
+
+  deleteTaskFromArchive(taskId: string) {
+    this.firestore
+    .collection('archive')
+    .doc(taskId)
+    .delete();
+  }
+
+  getBoardNameFromId(boardId: string): string | void{
+    let board = this.allBoards.find((board: any) => board.boardId == boardId);
+    if(!board) return;
+    return board.name;
   }
 
 }
--- src/app/kanban/task-details/task-details.component.ts
+++ src/app/kanban/task-details/task-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { take } from 'rxjs';
 import { Board } from 'src/models/board.class';
 import { MyTask } from 'src/models/task.class';
 import { PomodoroTimerService } from '../../../Services/pomodoro-timer.service';
@@ -16,7 +17,9 @@ export class TaskDetailsComponent implements OnInit {
   taskId: string = '';
   currentTask: any = new MyTask({dueDate: new Date()});
   activeBoard: any = new Board({});
+  loading = false;
 
+  taskInArchive = false;
   clockInit = [0, 10];
 
 
@@ -34,17 +37,84 @@ export class TaskDetailsComponent implements OnInit {
     .doc(this.taskId)
     .valueChanges()
     .subscribe((changes: any) => {
+      if(!changes) return;
       this.currentTask = new MyTask(changes);
+    });
+
+    this.firestore
+    .collection('archive')
+    .doc(this.taskId)
+    .valueChanges()
+    .subscribe((changes: any) => {
+      if(!changes) return;
+      this.currentTask = new MyTask(changes);
+      this.taskInArchive = true;
     })
   }
 
 
+  updateTaskInFirebase(task: any){
+    this.firestore
+    .collection('tasks')
+    .doc(task.taskId)
+    .update(task.toJSON());
+  }
+
+
   deleteTask(){
+    this.loading = true;
     if (this.timer.currentTaskId == this.taskId){
       this.timer.restartTimer();
     }
     this.firestore
     .collection('tasks')
+    .doc(this.taskId)
+    .delete()
+    .then(() => {
+      this.firestore
+      .collection('tasks', ref => ref.where('boardName', '==', this.currentTask.boardName)
+        .where('column', '==', this.currentTask.column))
+      .valueChanges({idField: 'taskId'})
+      .pipe(take(1))
+      .subscribe((changes) => {
+        let numTasksUpdated = 0;
+        for (let i = 0; i < changes.length; i++) {
+          const task: any = changes[i];
+          if(task.position > this.currentTask.position){
+            // Hier werden die Positionen für alle Aufgaben aktualisiert, die an Positionen nach 
+            // der gelöschten Aufgabe folgen: 
+            task.position --;
+            this.firestore
+            .collection('tasks')
+            .doc(task.taskId)
+            .update(task)
+            .then(() => {
+              numTasksUpdated++;
+              if(numTasksUpdated == changes.length - this.currentTask.position - 1){
+                // Dies wird ausgeführt, wenn alle Aufgaben deren Position geupdated wurden auch geupdated wurden:
+                this.loading = false; 
+                this.dialogRef.close();
+              }
+            });
+          }
+        }
+      })
+    });
+  }
+
+
+  addToArchive() {
+    this.firestore
+    .collection('archive')
+    .add(this.currentTask.toJSON());
+
+    this.deleteTask();
+  }
+
+
+  deleteTaskFromArchive() {
+    this.firestore
+    .collection('archive')
     .doc(this.taskId)
     .delete();
     this.dialogRef.close();
