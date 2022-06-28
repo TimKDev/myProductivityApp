ad781800dd65ff596b078783d5ead24e4de32554
Tim Kempkens <tim.kempkens@googlemail.com>
2022-06-28 11:41:47 +0200

    Delete All Task in Archive Button hinzugefügt

--- src/app/archive/dialog-delete-all/dialog-delete-all.component.ts
+++ src/app/archive/dialog-delete-all/dialog-delete-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialogRef } from '@angular/material/dialog';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-dialog-delete-all',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogDeleteAllComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogDeleteAllComponent>,
+    private firestore: AngularFirestore
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  deleteAllTasksFromArchive() {
+    this.firestore
+    .collection('archive')
+    .valueChanges({idField: 'taskId'})
+    .pipe(take(1))
+    .subscribe((changes: any) => {
+      changes.forEach((task: any) => {
+        this.firestore
+        .collection('archive')
+        .doc(task.taskId)
+        .delete()
+      });
+      this.dialogRef.close();
+    });
   }
 
 }
